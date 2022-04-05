5f63affbe185675f489c4530d88581222e6c28bb
wangjian <wangjian@yaoyanshe.com>
2022-04-06 01:09:54 +0800

    docs(boss): 模板整理

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+
 createApp(App)
   .use(store)
   .use(router)
@@ -14,3 +15,16 @@ createApp(App)
     locale: zhCn,
   })
   .mount("#app")
+
+export async function bootstrap() {
+  console.log("sub app bootstraped")
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function mount(props: any) {
+  console.log("props", props)
+}
+
+export async function unmount() {
+  console.log("sub app unmount")
+}
