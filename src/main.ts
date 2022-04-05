e40aefe458da52c261235ff98fd586a9a0636837
wangjian <wangjian@yaoyanshe.com>
2022-04-06 00:14:27 +0800

    feat: pre-commit测试

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import ElementPlus from "element-plus"
 import zhCn from "element-plus/es/locale/lang/zh-cn"
 import { createApp } from "vue"
 import App from "./App.vue"
-import router from "./router"
+import router from "./router";
 import store from "./store";
 createApp(App)
   .use(store)
